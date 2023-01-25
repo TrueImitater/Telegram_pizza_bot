7df3b2962affb45374039523250eaa1051411c09
Imitater <mr.peterkushnir@gmail.com>
2023-01-25 23:40:01 +0300

    Add cenz_filter | helper_func

--- bot_telegram.py
+++ bot_telegram.py
@@ -1,9 +1,16 @@
+import string
+import json
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.dispatcher.filters import Text
 from os import getenv
-from helper_info import HELP_TEXT, DESCRIPTION_TEXT
+from helper_info import HELP_TEXT, DESCRIPTION_TEXT, TIME_WORKING_TEXT
 
 bot = Bot(token=getenv('API_TOKEN'))
 dp = Dispatcher(bot)
+
+
+async def on_startup(_):
+    print("Bot has been started!")
 
 
 @dp.message_handler(commands=["start"])
@@ -23,6 +30,18 @@ async def description(message: types.Message):
     await bot.send_message(chat_id=message.from_id, text=DESCRIPTION_TEXT, parse_mode="HTML")
     await message.delete()
 
+
+@dp.message_handler(Text(equals="Время работы"))
+async def timeWorking(message: types.Message):
+    await message.reply(text=TIME_WORKING_TEXT, parse_mode="HTML")
+
+
+@dp.message_handler()
+async def word_filter(message: types.Message):
+    if {i.lower().translate(str.maketrans('', '', string.punctuation)) for i in message.text.split(' ')}.intersection(set(json.load(open('bad_words.json')))) != set():
+        await message.reply('Маты запрещены!')
+        await message.delete()
+
 if __name__ == "__main__":
     executor.start_polling(
-        dispatcher=dp, skip_updates=True)
+        dispatcher=dp, skip_updates=True, on_startup=on_startup)
